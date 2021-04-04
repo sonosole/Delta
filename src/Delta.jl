9b72b90b32518f1c8ab9342eaea2a3cb13226f14
sole <taiyang_zhao@163.com>
2021-04-04 12:18:53 +0800

    Update Delta.jl

--- src/Delta.jl
+++ src/Delta.jl
@@ -5,7 +5,7 @@ module Delta
 include("./kit/0-kit.jl")
 include("./basic/0-basic.jl")
 include("./block/0-block.jl")
-include("./loss/criterion.jl")
+include("./loss/0-loss.jl")
 include("./optimizer/optimizer.jl")
 include("./optimizer/update.jl")
 
