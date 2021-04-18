324fd2aff3c977a97912ee04942600b29841fa8e
sole <taiyang_zhao@163.com>
2021-04-19 00:54:41 +0800

    fix(conv1d)
    fix constructor of conv1d

--- src/block/conv/1-conv1d.jl
+++ src/block/conv/1-conv1d.jl
@@ -18,10 +18,15 @@ mutable struct conv1d <: Block
         stride::Int = 1,
         padding::Int = 0,
         type::Type=Array{Float32})
+
+        dtype = eltype(type)
         filterSize = ichannels * kernel
-        w = sqrt(2/filterSize) .* randn(ochannels, filterSize)
-        b = sqrt(2/filterSize) .* randn(ochannels,          1)
-        new(Variable{type}(w,true), Variable{type}(b,true), kernel, stride, padding)
+        amplitude = sqrt(dtype(2/filterSize))
+        w = amplitude * randn(dtype, ochannels, filterSize)
+        b = amplitude * randn(dtype, ochannels,          1)
+        new(Variable{type}(w,true,true,true),
+            Variable{type}(b,true,true,true),
+            kernel, stride, padding)
     end
 end
 
