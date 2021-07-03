ad8a5ef1e6f3685ffb5f2e12f56ea61d730f8e4d
sole <taiyang_zhao@163.com>
2021-07-03 16:02:31 +0800

    Update optimizer.jl
    1. Union{Float32,Float64} 改为 AbstractFloat
    2. learnRate 参数变量改名为 lr
    3. Momentum结构体中动量或者说惯性参量从 p 更名为 inertia
    4.  重构范数梯度剪裁的方法，主要是统一为 LpNormClip 方法，同时提供LNInfNormClip、
    LPInfNormClip、L0NormClip、L1NormClip、L2NormClip 几个常用方法

--- src/optimizer/optimizer.jl
+++ src/optimizer/optimizer.jl
@@ -10,17 +10,17 @@ abstract type Optimizer end
 
 
 mutable struct Descent <: Optimizer
-    lr::Union{Float32,Float64}
-    decay::Union{Float32,Float64}
+    lr::AbstractFloat
+    decay::AbstractFloat
     name::String
-    function Descent(;learnRate=1e-4,decay=1.0)
-        new(learnRate, decay, "Descent")
+    function Descent(;lr=1e-4,decay=1.0)
+        new(lr, decay, "Descent")
     end
 end
 
 # pretty printing
 function Base.show(io::IO, x::Descent)
-    print("Descent(learnRate=$(x.lr), decay=$(x.decay))")
+    print("Descent(lr=$(x.lr), decay=$(x.decay))")
 end
 
 
@@ -33,36 +33,33 @@ end
 
 mutable struct Momentum <: Optimizer
     v::Vector
-    lr::Union{Float32,Float64}
-    p::Union{Float32,Float64}
-    decay::Union{Float32,Float64}
+    lr::AbstractFloat
+    inertia::AbstractFloat
+    decay::AbstractFloat
     name::String
-    function Momentum(params::Vector{Variable};
-                      learnRate=1e-4,
-                      inertia=0.9,
-                      decay=1.0)
+    function Momentum(params::Vector{Variable}; lr=1e-4, inertia=0.9, decay=1.0)
         num = length(params)
         vel = Vector(undef,num)
         for i = 1:num
            vel[i] = Zeros(typeof(params[i].value), params[i].shape)
         end
-        new(vel, learnRate, inertia, decay, "Momentum")
+        new(vel, lr, inertia, decay, "Momentum")
     end
 end
 
 
 function Base.show(io::IO, x::Momentum)
-    print("Momentum(learnRate=$(x.lr), inertia=$(x.p), decay=$(x.decay))")
+    print("Momentum(lr=$(x.lr), inertia=$(x.p), decay=$(x.decay))")
 end
 
 
 function update(m::Momentum, params::Vector{Variable}; clipvalue=1e4)
     vel = m.v
     lr  = m.lr
-    p   = m.p
+    ρ   = m.inertia
     m.lr *= m.decay
     for i = 1:length(params)
-        @. vel[i] = p * vel[i] + clip(params[i].delta, clipvalue)
+        @. vel[i] = ρ * vel[i] + clip(params[i].delta, clipvalue)
         @. params[i].value -= lr * vel[i]
     end
 end
@@ -71,16 +68,16 @@ end
 mutable struct Adam <: Optimizer
     w1::Vector
     w2::Vector
-    lr::Union{Float32,Float64}
-    b1::Union{Float32,Float64}
-    b2::Union{Float32,Float64}
-    ϵ::Union{Float32,Float64}
+    lr::AbstractFloat
+    b1::AbstractFloat
+    b2::AbstractFloat
+    ϵ::AbstractFloat
     t::UInt
-    b1t::Union{Float32,Float64}
-    b2t::Union{Float32,Float64}
-    decay::Union{Float32,Float64}
+    b1t::AbstractFloat
+    b2t::AbstractFloat
+    decay::AbstractFloat
     name::String
-    function Adam(params::Vector{Variable}; learnRate=1e-4, b1=0.9, b2=0.996, epsilon=1e-8, decay=1.0)
+    function Adam(params::Vector{Variable}; lr=1e-4, b1=0.9, b2=0.996, epsilon=1e-8, decay=1.0)
         num = length(params)
         w1  = Vector(undef,num)
         w2  = Vector(undef,num)
@@ -88,13 +85,13 @@ mutable struct Adam <: Optimizer
             w1[i] = Zeros(typeof(params[i].value), params[i].shape)
             w2[i] = Zeros(typeof(params[i].value), params[i].shape)
         end
-        new(w1,w2,learnRate, b1, b2, epsilon, 0, 1.0, 1.0, decay, "Adam")
+        new(w1,w2,lr, b1, b2, epsilon, 0, 1.0, 1.0, decay, "Adam")
     end
 end
 
 
 function Base.show(io::IO, x::Adam)
-    print("Adam(learnRate=$(x.lr), β₁=$(x.b1), β₂=$(x.b2), ϵ=$(x.ϵ) decay=$(x.decay))");
+    print("Adam(lr=$(x.lr), β₁=$(x.b1), β₂=$(x.b2), ϵ=$(x.ϵ) decay=$(x.decay))");
 end
 
 
@@ -115,8 +112,8 @@ function update(a::Adam, params::Vector{Variable}; clipvalue=1.0)
 
     for i = 1:length(params)
         μ = sqrt(1-b₂ᵗ) / (1-b₁ᵗ) * lr
-        @.  params[i].delta = clip(params[i].delta, clipvalue)
         ∇ = params[i].delta
+        @. ∇ = clip(∇, clipvalue)
         @. w₁[i] = b₁ * w₁[i] + (1-b₁) * ∇
         @. w₂[i] = b₂ * w₂[i] + (1-b₂) * ∇ * ∇
         @. params[i].value -= μ * w₁[i] / sqrt(w₂[i] + ϵ)
@@ -131,11 +128,67 @@ function decay(params::Vector{Variable}; ratio=0.999)
 end
 
 
-function normclip(gradient, clipvalue)
-    L2NormVal  = sqrt( sum(gradient .^ 2) / length(gradient) )
-    Normalizer = clipvalue / L2NormVal
-    if L2NormVal > clipvalue
-        gradient .*= Normalizer
+function L2NormClip(x::AbstractArray, clipvalue)
+    pnorm = sqrt( sum( x .^ 2 ) )
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
     end
-    return gradient
+    return x
+end
+
+
+function L1NormClip(x::AbstractArray, clipvalue)
+    pnorm = sum( abs.(x) )
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
+    end
+    return x
+end
+
+
+function L0NormClip(x::AbstractArray, clipvalue)
+    pnorm = sum(x .!= 0.0)
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
+    end
+    return x
+end
+
+
+function LPInfNormClip(x::AbstractArray, clipvalue)
+    pnorm = maximum(abs.(x))
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
+    end
+    return x
+end
+
+
+function LNInfNormClip(x::AbstractArray, clipvalue)
+    pnorm = minimum(abs.(x))
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
+    end
+    return x
+end
+
+
+function LpNormClip(x::AbstractArray, clipvalue; order::Union{Int,String}=2)
+    order==2 && return L2NormClip(x, clipvalue)
+    order==1 && return L1NormClip(x, clipvalue)
+    order==0 && return L0NormClip(x, clipvalue)
+    order=="inf"  && return LPInfNormClip(x, clipvalue)
+    order=="-inf" && return LNInfNormClip(x, clipvalue)
+
+    pnorm = sum( abs.(x) .^ order ) ^ (1/order)
+    scale = clipvalue / pnorm
+    if pnorm > clipvalue
+        x .*= scale
+    end
+    return x
 end
