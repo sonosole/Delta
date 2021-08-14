0164fb1a3c9f75348cfbab82173bbe2d072225a8
sole <taiyang_zhao@163.com>
2021-08-14 16:50:47 +0800

    Update optimizer.jl
    添加 lrarray 方法，达到控制学习率的目的，目前有cos exp linear 三种

--- src/optimizer/optimizer.jl
+++ src/optimizer/optimizer.jl
@@ -22,6 +22,7 @@ export L1NormClip
 export L0NormClip
 export LPInfNormClip
 export LNInfNormClip
+export lrarray
 
 
 function decay(params::Vector{Variable}; ratio=0.999)
@@ -93,4 +94,61 @@ function LpNormClip(x::AbstractArray, clipvalue; order::Union{Int,String}=2)
         x .*= scale
     end
     return x
+end
+
+
+"""
+    lrarray(init,final,steps;func="exp") -> Array{Float32,1}
+# Arguments
+- ` init`: initial learning rate
+- `final`: final learning rate
+- `steps`: steps to change from `init` to `final`
+- ` func`: function used to change the learning rate. (e.g. exp/cos/linear)
+"""
+function lrarray(init,final,steps;func="exp")
+    func=="exp"    && return lrarrayexp(init,final,steps)
+    func=="cos"    && return lrarraycos(init,final,steps)
+    func=="linear" && return lrarraylinear(init,final,steps)
+end
+
+
+function lrarrayexp(i,f,n)
+    if n == 1
+        return [i]
+    else
+        lr = zeros(Float32,n)
+        α  = log(f/i)/(n-1)
+        for x = 1:n
+            lr[x] = i * exp(α * (x-1))
+        end
+        return lr
+    end
+end
+
+function lrarraylinear(i,f,n)
+    if n == 1
+        return [i]
+    else
+        lr = zeros(Float32,n)
+        α  = (f-i)/(n-1)
+        for x = 1:n
+            lr[x] = α*(x-1) + i
+        end
+        return lr
+    end
+end
+
+
+function lrarraycos(i,f,n)
+    if n == 1
+        return [i]
+    else
+        lr = zeros(Float32,n)
+        α  = pi/2/(n-1)
+        Δ  = (i-f)
+        for x = 1:n
+            lr[x] = cos(α*(x-1)) * Δ + f
+        end
+        return lr
+    end
 end
