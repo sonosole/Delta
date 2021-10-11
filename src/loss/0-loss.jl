fa0b6e86560e224100046c736d118ffb0ddf0bb7
sole <taiyang_zhao@163.com>
2021-10-12 00:22:34 +0800

    feat: seq loss

--- src/loss/0-loss.jl
+++ src/loss/0-loss.jl
@@ -1,3 +1,3 @@
 include("./1-criterion.jl")
 include("./2-ctc-cpu.jl")
-# include("./3-tcs-cpu.jl")
+include("./3-tcs-cpu.jl")
---
+++ src/loss/3-tcs-cpu.jl
@@ -0,0 +1,126 @@
+export TCS
+export TCSGreedySearch
+export CRNN_Batch_TCS_With_Softmax
+
+
+"""
+    TCS(p::Array{T,2}, seq) where T -> target, lossvalue
+# inputs
+`p`: probability of softmax output\n
+`seq`: label seq like [1 2 3 1 2 3 1 2 6 1 2 5 1], of which 1 is background state 2 is foreground state.
+       If `p` has no label (e.g. pure noise or oov) then `seq` is [1]
+
+# outputs
+`target`: target of softmax's output\n
+`lossvalue`: negative log-likelyhood
+"""
+function TCS(p::Array{TYPE,2}, seq) where TYPE
+    Log0 = LogZero(TYPE)   # approximate -Inf of TYPE
+    ZERO = TYPE(0)         # typed zero,e.g. Float32(0)
+    S, T = size(p)         # assert p is a 2-D tensor
+    L = length(seq)        # topology length
+    a = fill!(Array{TYPE,2}(undef,L,T), Log0)  # 𝜶 = p(s[k,t], x[1:t]), k in TCS topology's indexing
+    b = fill!(Array{TYPE,2}(undef,L,T), Log0)  # 𝛃 = p(x[t+1:T] | s[k,t]), k in TCS topology's indexing
+    r = fill!(Array{TYPE,2}(undef,S,T), ZERO)  # 𝜸 = p(s[k,t] | x[1:T]), k in softmax's indexing
+
+	if L>1
+		a[1,1] = log(p[seq[1],1])
+		a[2,1] = log(p[seq[2],1])
+		b[L-1,T] = ZERO
+		b[L-0,T] = ZERO
+	else
+		a[1,1] = log(p[seq[1],1])
+		b[L,T] = ZERO
+	end
+
+    # --- forward in log scale ---
+	for t = 2:T
+	    for s = 1:L
+	        if s!=1
+				R = mod(s,3)
+	            if R==1 || s==2 || R==0
+	                a[s,t] = LogSum2Exp(a[s,t-1], a[s-1,t-1])
+	            elseif R==2
+	                a[s,t] = LogSum3Exp(a[s,t-1], a[s-1,t-1], a[s-2,t-1])
+	            end
+	        else
+	            a[s,t] = a[s,t-1]
+	        end
+	        a[s,t] += log(p[seq[s],t])
+	    end
+	end
+
+    # --- backward in log scale ---
+	for t = T-1:-1:1
+		for s = L:-1:1
+			Q = b[s,t+1] + log(p[seq[s],t+1])
+			if s!=L
+				R = mod(s,3)
+				V = b[s+1,t+1] + log(p[seq[s+1],t+1])
+				if R==1 || R==2 || s==L-1
+					b[s,t] = LogSum2Exp(Q, V)
+				elseif R==0
+					b[s,t] = LogSum3Exp(Q, V, b[s+2,t+1] + log(p[seq[s+2],t+1]))
+				end
+			else
+				b[s,t] = Q
+			end
+		end
+	end
+
+    # loglikely of TCS
+    logsum = LogSum3Exp(Log0, a[1,1] + b[1,1], a[2,1] + b[2,1])
+
+    # log weight --> normal probs
+	g = exp.((a + b) .- logsum)
+
+    # Reduce First line
+    r[seq[1],:] .+= g[1,:]
+    # Reduce other lines
+    for n = 1:div(L-1,3)
+        s = 3*n
+        r[seq[s-1],:] .+= g[s-1,:]  # reduce forground states
+        r[seq[s  ],:] .+= g[s,  :]  # reduce labels' states
+        r[seq[s+1],:] .+= g[s+1,:]  # reduce background state
+    end
+
+    return r, -logsum
+end
+
+
+function TCSGreedySearch(x::Array)
+	# Backgroud  --> 1 index
+    # Foreground --> 2 index
+    hyp = []
+    idx = argmax(x,dims=1)
+    for i = 1:length(idx)
+        maxid = idx[i][1]
+        if !((i!=1 && idx[i][1]==idx[i-1][1]) || (idx[i][1]==1) || (idx[i][1]==2))
+            push!(hyp, maxid)
+        end
+    end
+    return hyp
+end
+
+
+function CRNN_Batch_TCS_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector) where T
+    featdims, timesteps, batchsize = size(var)
+    probs = softmax(var.value; dims=1)
+    gamma = zero(var.value)
+    loglikely = zeros(T, batchsize)
+
+    Threads.@threads for b = 1:batchsize
+        gamma[:,:,b], loglikely[b] = TCS(probs[:,:,b], seqlabels[b])
+        loglikely[b] /= length(seqlabels[b])
+    end
+
+    if var.backprop
+        function CRNN_Batch_TCS_With_Softmax_Backward()
+            if need2computeδ!(var)
+                var.delta += probs - gamma
+            end
+        end
+        push!(graph.backward, CRNN_Batch_TCS_With_Softmax_Backward)
+    end
+    return sum(loglikely)/batchsize
+end
