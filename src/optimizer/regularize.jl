3b6108273d229077265b288eb566224663523260
sole <taiyang_zhao@163.com>
2021-10-13 23:28:48 +0800

    Update regularize.jl
    去掉数组的类型约束

--- src/optimizer/regularize.jl
+++ src/optimizer/regularize.jl
@@ -5,11 +5,11 @@ export sparsityof
 export sparsitypair
 
 """
-    regularize(Ws::Vector{<:AbstractArray}; lambda::AbstractFloat, method::String="L2")
+    regularize(Ws::Vector; lambda::AbstractFloat, method::String="L2")
 
 apply L1 or L2 regularization to unbiased weights, recurrent weights not included
 """
-function regularize(Ws::Vector{<:AbstractArray}; lambda::AbstractFloat, method::String="L2")
+function regularize(Ws::Vector; lambda::AbstractFloat, method::String="L2")
     method = uppercase(method)
     method=="L2" && return L2Regularize(Ws, lambda)
     method=="L1" && return L1Regularize(Ws, lambda)
@@ -17,56 +17,56 @@ end
 
 
 """
-    L1Regularize(Ws::Vector{<:AbstractArray}, lambda::AbstractFloat)
+    L1Regularize(Ws::Vector, λ::AbstractFloat)
 
 apply L1 regularization to unbiased weights, recurrent weights not included
 """
-function L1Regularize(Ws::Vector{<:AbstractArray}, λ::AbstractFloat)
-    for w in Ws
+function L1Regularize(Ws::Vector, λ::AbstractFloat)
+    Threads.@threads for w in Ws
         @. w += -λ * sign(w)
     end
 end
 
 """
-    L2Regularize(Ws::Vector{<:AbstractArray}, lambda::AbstractFloat)
+    L2Regularize(Ws::Vector, λ::AbstractFloat)
 
 apply L2 regularizationto unbiased weights, recurrent weights not included
 """
-function L2Regularize(Ws::Vector{<:AbstractArray}, λ::AbstractFloat)
-    for w in Ws
+function L2Regularize(Ws::Vector, λ::AbstractFloat)
+    Threads.@threads for w in Ws
         @. w += -λ * w
     end
 end
 
 
 """
-    sparsityof(x::AbstractArray, a::AbstractFloat) -> sparserRatio
+    sparsityof(x, a::AbstractFloat) -> sparserRatio
 
 calculate the sparsity of Array `x` at given threshold `a > 0`
 """
-function sparsityof(x::AbstractArray, a::AbstractFloat)
-    @assert a>0.0 "the threshold should > 0, but got $a "
+function sparsityof(x, a::AbstractFloat)
+    @assert a>0.0 "the threshold should > 0, but got $a"
     return sum(abs.(x) .< a) / length(x)
 end
 
 
 """
-    sparsitypair(x::AbstractArray, a::AbstractFloat) -> (sparsity::Int, length(x)::Int)
+    sparsitypair(x, a::AbstractFloat) -> (sparsity::Int, length(x)::Int)
 
-calculate the #sparsity of AbstractArray `x` at given threshold `a > 0` and return (sparsity, length(x))
+calculate the #sparsity of Array `x` at given threshold `a > 0` and return (sparsity, length(x))
 """
-function sparsitypair(x::AbstractArray, a::AbstractFloat)
+function sparsitypair(x, a::AbstractFloat)
     @assert a>0.0 "the threshold should > 0, but got $a"
     return sum(abs.(x) .< a), length(x)
 end
 
 
 """
-    sparsityof(xs::Vector{<:AbstractArray}, a::AbstractFloat) -> sparserRatio
+    sparsityof(xs::Vector, a::AbstractFloat) -> sparserRatio
 
-calculate the sparsity of Vector{AbstractArray} `xs` at given threshold `a > 0`
+calculate the sparsity of a Vector of Arrays `xs` at given threshold `a > 0`
 """
-function sparsityof(xs::Vector{<:AbstractArray}, a::AbstractFloat)
+function sparsityof(xs::Vector, a::AbstractFloat)
     @assert a>0.0 "the threshold should > 0, but got $a"
     S = 0
     T = 0
