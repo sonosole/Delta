317eb87844334b5289a72a51edf610b23027505e
sole <taiyang_zhao@163.com>
2021-10-20 23:48:57 +0800

    Create 1-SGD.jl
    replace Decent.jl

---
+++ src/optimizer/1-SGD.jl
@@ -0,0 +1,20 @@
+mutable struct SGD <: Optimizer
+    lr::AbstractFloat
+    lrdecay::AbstractFloat
+    name::String
+    function SGD(;lr=1e-4, lrdecay=1.0)
+        new(lr, decay, "SGD")
+    end
+end
+
+# pretty printing
+function Base.show(io::IO, S::SGD)
+    print("SGD(lr=$(S.lr), lrdecay=$(S.lrdecay))")
+end
+
+
+function update!(s::SGD, params::Vector{Variable})
+    lrate = s.lr
+    s.lr *= s.decay
+    update!(params, lrate)
+end
