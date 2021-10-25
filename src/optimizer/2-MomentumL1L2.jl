8d057fdf5b610ceb5dbe9f5b9dd8b1ce5f737017
Sole <30993566+sonosole@users.noreply.github.com>
2021-10-25 10:57:11 +0800

    Update 2-MomentumL1L2.jl
    fix show

--- src/optimizer/2-MomentumL1L2.jl
+++ src/optimizer/2-MomentumL1L2.jl
@@ -19,7 +19,7 @@ end
 
 
 function Base.show(io::IO, O::MomentumL1L2)
-    print("MomentumL1L2(lr=$(O.lr), inertia=$(O.p), L2decay=$(O.L2decay))")
+    print("MomentumL1L2(lr=$(O.lr), inertia=$(O.inertia), L2decay=$(O.L2decay))")
 end
 
 
