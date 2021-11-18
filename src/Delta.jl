907fa141d3098869ba5097b003d5141f1cd4ebd5
Sole <30993566+sonosole@users.noreply.github.com>
2021-11-18 10:14:35 +0800

    Update Delta.jl
    import `Statistics.mean` to solve conficts with `Delta.mean(::Variable;dims=...)`

--- src/Delta.jl
+++ src/Delta.jl
@@ -2,6 +2,9 @@ Base.__precompile__(true)
 
 module Delta
 
+using Statistics: mean, std
+import Statistics.mean
+
 include("./kit/0-kit.jl")
 include("./basic/0-basic.jl")
 include("./block/0-block.jl")
