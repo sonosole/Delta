86116fd7139b4c90532c705bde899db52c07628b
sole <taiyang_zhao@163.com>
2021-11-20 21:00:47 +0800

    Update 5-ResDense.jl
    simplify. delete one linear layer inside

--- src/block/fc/5-ResDense.jl
+++ src/block/fc/5-ResDense.jl
@@ -1,76 +1,32 @@
 mutable struct ResDense <: Block
     blocks::Vector
-    function ResDense(i::Int, n::Int, m::Int; type::Type=Array{Float32})
-        l1 = linear(i,n, type=type)
-        l2 = linear(n,m, type=type)
-        l3 = linear(m,i, type=type)
-        new([l1, l2, l3])
+    function ResDense(i::Int, m::Int; type::Type=Array{Float32})
+        l1 = linear(i, m, type=type)
+        l2 = linear(m, i, type=type)
+        new([l1, l2])
     end
     function ResDense()
-        new(Vector(undef,3))
+        new(Vector(undef,2))
     end
 end
 
-function Base.show(io::IO, c::ResDense)
-    print(io,  "ResDense\n(\n      ")
-    join(io, c.blocks, "\n      ")
-    print(io,               "\n)")
-end
-
-Base.length(c::ResDense)     = 3
-Base.lastindex(c::ResDense)  = 3
-Base.firstindex(c::ResDense) = 1
-Base.getindex(c::ResDense, k...)     =  c.blocks[k...]
-Base.setindex!(c::ResDense, v, k...) = (c.blocks[k...] = v)
-Base.iterate(c::ResDense, i=1) = i>3 ? nothing : (c[i], i+1)
-
-function paramsof(m::ResDense)
-    params = Vector{Variable}(undef,0)
-    for i = 1:length(m)
-        append!(params, paramsof(m[i]))
-    end
-    return params
-end
-
-function xparamsof(m::ResDense)
-    xparams = Vector{XVariable}(undef,0)
-    for i = 1:length(m)
-        append!(xparams, xparamsof(m[i]))
-    end
-    return xparams
-end
-
-function nparamsof(model::ResDense)
-    nparams = 0
-    for m in model
-        nparams += nparamsof(m)
-    end
-    return nparams
-end
-
-function bytesof(model::ResDense, unit::String="MB")
-    n = nparamsof(model) * elsizeof(model[1].w)
-    return blocksize(n, uppercase(unit))
-end
+@extend(ResDense, blocks)
 
 function clone(this::ResDense; type::Type=Array{Float32})
     cloned = ResDense()
     cloned[1] = clone(this[1], type=type)
     cloned[2] = clone(this[2], type=type)
-    cloned[3] = clone(this[3], type=type)
     return cloned
 end
 
 function forward(m::ResDense, x0)
     x1 = relu(forward(m[1], x0))
-    x2 = relu(forward(m[2], x1))
-    x3 = x0 + forward(m[3], x2)
-    return relu(x3)
+    x2 = x0 + forward(m[2], x1)
+    return relu(x2)
 end
 
 function predict(m::ResDense, x0)
     x1 = relu(predict(m[1], x0))
-    x2 = relu(predict(m[2], x1))
-    x3 = x0 + predict(m[3], x2)
-    return relu(x3)
+    x2 = x0 + predict(m[2], x1)
+    return relu(x2)
 end
