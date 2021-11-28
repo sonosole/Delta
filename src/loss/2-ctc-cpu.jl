fc38e0df4f6aba704461e5bac5a299e2e593c7ec
sole <taiyang_zhao@163.com>
2021-11-29 02:02:22 +0800

    refactor: optional blank indexing and weight for whole seq

--- src/loss/2-ctc-cpu.jl
+++ src/loss/2-ctc-cpu.jl
@@ -23,16 +23,16 @@ end
 
 
 """
-    CTC(p::Array{T,2}, seq) where T -> (target, lossvalue)
-# inputs
-`p`: probability of softmax output\n
-`seq`: label seq like [2 3 6 5], 1 is blank, so minimum of it is 2. If `p` has no label (e.g. pure noise or oov) then `seq` is []
-
-# outputs
-`target`: target of softmax's output\n
-`lossvalue`: negative log-likelyhood
+    CTC(p::Array{T,2}, seq; blank=1) where T -> (target, lossvalue)
+# Inputs
+    p   : probability of softmax output\n
+    seq : label seq like [9 3 6 15] which contains no blank. If p 
+          has no label (e.g. pure noise or oov) then seq is []
+# Outputs
+    target    : target of softmax's output\n
+    lossvalue : negative log-likelyhood
 """
-function CTC(p::Array{TYPE,2}, seq) where TYPE
+function CTC(p::Array{TYPE,2}, seq; blank=1) where TYPE
     Log0 = LogZero(TYPE)   # approximate -Inf of TYPE
     ZERO = TYPE(0)         # typed zero,e.g. Float32(0)
     S, T = size(p)         # assert p is a 2-D tensor
@@ -40,8 +40,8 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
     r = fill!(Array{TYPE,2}(undef,S,T), ZERO)    # 𝜸 = p(s[k,t] | x[1:T]), k in softmax's indexing
 
     if L == 1
-        r[1,:] .= TYPE(1)
-        return r, - sum(log.(p[1,:]))
+        r[blank,:] .= TYPE(1)
+        return r, - sum(log.(p[blank,:]))
     end
 
     a = fill!(Array{TYPE,2}(undef,L,T), Log0)    # 𝜶 = p(s[k,t], x[1:t]), k in CTC topology's indexing
@@ -58,9 +58,9 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
         for s = first:lasst
             i = div(s,2);
             if s==1
-                a[s,t] = a[s,t-1] + log(p[1,t])
+                a[s,t] = a[s,t-1] + log(p[blank,t])
             elseif mod(s,2)==1
-                a[s,t] = LogSum2Exp(a[s,t-1], a[s-1,t-1]) + log(p[1,t])
+                a[s,t] = LogSum2Exp(a[s,t-1], a[s-1,t-1]) + log(p[blank,t])
             elseif s==2
                 a[s,t] = LogSum2Exp(a[s,t-1], a[s-1,t-1]) + log(p[seq[i],t])
             elseif seq[i]==seq[i-1]
@@ -79,15 +79,15 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
             i = div(s,2)
             j = div(s+1,2)
             if s==L
-                b[s,t] = b[s,t+1] + log(p[1,t+1])
+                b[s,t] = b[s,t+1] + log(p[blank,t+1])
             elseif mod(s,2)==1
-                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[1,t+1]), b[s+1,t+1] + log(p[seq[j],t+1]))
+                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[blank,t+1]), b[s+1,t+1] + log(p[seq[j],t+1]))
             elseif s==L-1
-                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[1,t+1]))
+                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[blank,t+1]))
             elseif seq[i]==seq[i+1]
-                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[1,t+1]))
+                b[s,t] = LogSum2Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[blank,t+1]))
             else
-                b[s,t] = LogSum3Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[1,t+1]), b[s+2,t+1] + log(p[seq[i+1],t+1]))
+                b[s,t] = LogSum3Exp(b[s,t+1] + log(p[seq[i],t+1]), b[s+1,t+1] + log(p[blank,t+1]), b[s+2,t+1] + log(p[seq[i+1],t+1]))
             end
         end
     end
@@ -95,13 +95,13 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
     logsum = LogSum3Exp(Log0, a[1,1] + b[1,1], a[2,1] + b[2,1])
     g = exp.((a + b) .- logsum)
 
-    # reduce first line
-    r[1,:] .+= g[1,:]
-    # reduce rest lines
+    # reduce first line of g
+    r[blank,:] .+= g[1,:]
+    # reduce rest lines of g
     for n = 1:length(seq)
         s = n<<1
         r[seq[n],:] .+= g[s,  :]
-        r[1     ,:] .+= g[s+1,:]
+        r[blank, :] .+= g[s+1,:]
     end
 
     return r, -logsum
@@ -109,17 +109,17 @@ end
 
 
 """
-    CTCGreedySearch(x::Array)
-remove repeats and blanks of argmax(x, dims=1)
+    CTCGreedySearch(x::Array; blank=1, dims=1)
+remove repeats and blanks of argmax(x, dims=dims)
 """
-function CTCGreedySearch(x::Array)
-    # blank --> 1
-    hyp = Vector{Int}(undef,0)
-    idx = argmax(x,dims=1)
+function CTCGreedySearch(x::Array; blank=1, dims=1)
+    hyp = Vector{Int}(undef, 0)
+    idx = argmax(x, dims=dims)
     for i = 1:length(idx)
-        maxid = idx[i][1]
-        if !((i!=1 && idx[i][1]==idx[i-1][1]) || (idx[i][1]==1))
-            push!(hyp, maxid)
+        previous = idx[i-1][1]
+        current  = idx[i  ][1]
+        if !((i!=1 && current==previous) || (current==blank))
+            push!(hyp, current)
         end
     end
     return hyp
@@ -132,15 +132,19 @@ end
 `var`: 2-D Variable, input sequence.\n
 `seq`: 1-D Array, input sequence's label.
 """
-function DNN_CTC_With_Softmax(var::Variable{Array{T}}, seq) where T
+function DNN_CTC_With_Softmax(var::Variable{Array{T}}, seq; blank=1, weight=1.0) where T
     # for case batchsize==1
     p = softmax(var.value; dims=1)
     L = length(seq) * 2 + 1
-    r, loglikely = CTC(p, seq)
+    r, loglikely = CTC(p, seq, blank=blank)
     if var.backprop
         function DNN_CTC_With_Softmax_Backward()
             if need2computeδ!(var)
-                var.delta += p - r
+                if weight==1.0
+                    var.delta += p - r
+                else
+                    var.delta += (p - r) .* weight
+                end
             end
         end
         push!(graph.backward, DNN_CTC_With_Softmax_Backward)
@@ -157,7 +161,7 @@ end
 `inputLengths`: 1-D Array which records each input sequence's length.\n
 `labelLengths`: 1-D Array which records input sequence label's length.
 """
-function DNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seq, inputLengths, labelLengths) where T
+function DNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seq, inputLengths, labelLengths; blank=1, weight=1.0) where T
     batchsize = length(inputLengths)
     loglikely = zeros(T, batchsize)
     probs = softmax(var.value; dims=1)
@@ -168,14 +172,18 @@ function DNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seq, inputLengths, 
     Threads.@threads for b = 1:batchsize
         IDI = sidI[b]:eidI[b]
         IDL = sidL[b]:eidL[b]
-        gamma[:,IDI], loglikely[b] = CTC(probs[:,IDI], seq[IDL])
+        gamma[:,IDI], loglikely[b] = CTC(probs[:,IDI], seq[IDL], blank=blank)
         loglikely[b] /= length(IDL) * 2 + 1
     end
 
     if var.backprop
         function DNN_Batch_CTC_With_Softmax_Backward()
             if need2computeδ!(var)
-                var.delta += probs - gamma
+                if weight==1.0
+                    var.delta += probs - gamma
+                else
+                    var.delta += (probs - gamma) .* weight
+                end
             end
         end
         push!(graph.backward, DNN_Batch_CTC_With_Softmax_Backward)
@@ -192,7 +200,7 @@ end
 `inputLengths`: 1-D Array which records each input's length.\n
 `labelLengths`: 1-D Array which records all labels' length.\n
 """
-function RNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector, inputLengths, labelLengths) where T
+function RNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector, inputLengths, labelLengths; blank=1, weight=1.0) where T
     batchsize = length(inputLengths)
     loglikely = zeros(T, batchsize)
     probs = zero(var.value)
@@ -202,14 +210,18 @@ function RNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector, 
         Tᵇ = inputLengths[b]
         Lᵇ = labelLengths[b]
         probs[:,1:Tᵇ,b] = softmax(var.value[:,1:Tᵇ,b]; dims=1)
-        gamma[:,1:Tᵇ,b], loglikely[b] = CTC(probs[:,1:Tᵇ,b], seqlabels[b])
+        gamma[:,1:Tᵇ,b], loglikely[b] = CTC(probs[:,1:Tᵇ,b], seqlabels[b], blank=blank)
         loglikely[   b] /= Lᵇ * 2 + 1
     end
 
     if var.backprop
         function RNN_Batch_CTC_With_Softmax_Backward()
             if need2computeδ!(var)
-                var.delta += probs - gamma
+                if weight==1.0
+                    var.delta += probs - gamma
+                else
+                    var.delta += (probs - gamma) .* weight
+                end
             end
         end
         push!(graph.backward, RNN_Batch_CTC_With_Softmax_Backward)
@@ -224,21 +236,25 @@ end
 `var`: 3-D Variable (featdims,timesteps,batchsize), resulted by a batch of padded input sequence.\n
 `seqlabels`: a vector contains a batch of 1-D Array labels.\n
 """
-function CRNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector) where T
+function CRNN_Batch_CTC_With_Softmax(var::Variable{Array{T}}, seqlabels::Vector; blank=1, weight=1.0) where T
     featdims, timesteps, batchsize = size(var)
     probs = softmax(var.value; dims=1)
     gamma = zero(var.value)
     loglikely = zeros(T, batchsize)
 
     Threads.@threads for b = 1:batchsize
-        gamma[:,:,b], loglikely[b] = CTC(probs[:,:,b], seqlabels[b])
+        gamma[:,:,b], loglikely[b] = CTC(probs[:,:,b], seqlabels[b], blank=blank)
         loglikely[b] /= length(seqlabels[b])*2 + 1
     end
 
     if var.backprop
         function CRNN_Batch_CTC_With_Softmax_Backward()
             if need2computeδ!(var)
-                var.delta += probs - gamma
+                if weight==1.0
+                    var.delta += probs - gamma
+                else
+                    var.delta += (probs - gamma) .* weight
+                end
             end
         end
         push!(graph.backward, CRNN_Batch_CTC_With_Softmax_Backward)
