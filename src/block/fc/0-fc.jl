651cda5c5c7570b020437ca802fdd3a7c7a898f1
sole <taiyang_zhao@163.com>
2021-12-05 20:55:19 +0800

    feat(Res0dWithBN): Res0d With BatchNorm0d

--- src/block/fc/0-fc.jl
+++ src/block/fc/0-fc.jl
@@ -3,7 +3,7 @@ export dense
 export MLP
 export maxout
 export affine
-export ResDense
+export Res0d, Res0dWithBN
 export SelfLoopResNet
 export SelfLoopCumulativeResNet
 export MeanNormResDense
@@ -13,6 +13,7 @@ include("./2-dense.jl")
 include("./3-maxout.jl")
 include("./4-affine.jl")
 include("./5-Res0d.jl")
+include("./5-Res0dWithBN.jl")
 include("./6-SelfLoopResNet.jl")
 include("./7-SelfLoopCumulativeResNet.jl")
 include("./8-MeanNormResDense.jl")
