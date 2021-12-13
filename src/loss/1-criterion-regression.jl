a1febe9c893bc2ff9e19d27c053b28c3c973fd6a
sole <taiyang_zhao@163.com>
2021-12-14 02:44:02 +0800

    refactor(criterion): refactor loss with _sum and _mean methods

--- src/loss/1-criterion-regression.jl
+++ src/loss/1-criterion-regression.jl
@@ -33,9 +33,9 @@ function mae(x::Variable{T}, label::Variable{T}) where T
     return y
 end
 
-maeLoss(x::Variable{T}, label::Variable{T}) where T = loss( mae(x, label) )
+maeLoss(x::Variable{T}, label::Variable{T}; reduction::String="sum") where T = loss( mae(x, label), reduction=reduction )
+L1Loss(x::Variable{T},  label::Variable{T}; reduction::String="sum") where T = loss( mae(x, label), reduction=reduction )
 maeCost(x::Variable{T}, label::Variable{T}) where T = cost( mae(x, label) )
-L1Loss(x::Variable{T},  label::Variable{T}) where T = loss( mae(x, label) )
 L1Cost(x::Variable{T},  label::Variable{T}) where T = cost( mae(x, label) )
 
 
@@ -62,16 +62,16 @@ function mse(x::Variable{T}, label::Variable{T}) where T
     return y
 end
 
-mseLoss(x::Variable{T}, label::Variable{T}) where T = loss( mse(x, label) )
+mseLoss(x::Variable{T}, label::Variable{T}; reduction::String="sum") where T = loss( mse(x, label), reduction=reduction )
+L2Loss(x::Variable{T},  label::Variable{T}; reduction::String="sum") where T = loss( mse(x, label), reduction=reduction )
 mseCost(x::Variable{T}, label::Variable{T}) where T = cost( mse(x, label) )
-L2Loss(x::Variable{T},  label::Variable{T}) where T = loss( mse(x, label) )
 L2Cost(x::Variable{T},  label::Variable{T}) where T = cost( mse(x, label) )
 
 
 """
     Lp(x::Variable{T}, label::Variable{T}; p=3) -> y::Variable{T}
 
-absolute error's p-th power between each element in the input `x` and target `label`. Also called LpLoss. i.e. ⤦\n
+absolute error's `p`-th power between each element in the input `x` and target `label`. Also called LpLoss. i.e. ⤦\n
     y = |xᵢ - lᵢ|ᵖ
 """
 function Lp(x::Variable{T}, label::Variable{T}; p=3) where T
@@ -93,5 +93,5 @@ function Lp(x::Variable{T}, label::Variable{T}; p=3) where T
     return y
 end
 
-LpLoss(x::Variable{T}, label::Variable{T}; p=3) where T = loss( Lpnorm(x, label; p=p) )
-LpCost(x::Variable{T}, label::Variable{T}; p=3) where T = cost( Lpnorm(x, label; p=p) )
+LpLoss(x::Variable{T}, label::Variable{T}; p=3, reduction::String="sum") where T = loss( Lp(x, label; p=p), reduction=reduction )
+LpCost(x::Variable{T}, label::Variable{T}; p=3) where T = cost( Lp(x, label; p=p) )
--- src/loss/1-criterion.jl
+++ src/loss/1-criterion.jl
@@ -2,19 +2,65 @@ export loss
 export cost
 
 
-function loss(x::Variable{T}) where T
+# Internal function
+function _sum(x::Variable{T}) where T
     y = Variable{T}([sum(ᵛ(x))], x.backprop)
     if x.backprop
-        function lossBackward()
-            if need2computeδ!(x) δ(x) .+= δ(y) end
-            ifNotKeepδThenFreeδ!(y);
+        function _sumBackward()
+            if need2computeδ!(x)
+                δ(x) .+= δ(y)
+            end
+            ifNotKeepδThenFreeδ!(y)
         end
-        push!(graph.backward, lossBackward)
+        push!(graph.backward, _sumBackward)
     end
     return y
 end
 
 
+# Internal function
+function _mean(x::Variable{T}) where T
+    n = eltype(x)(1) / prod(size(x))
+    μ = Variable{T}([sum(ᵛ(x)) * n], x.backprop)
+    if x.backprop
+        function _meanBackward()
+            if need2computeδ!(x)
+                δ(x) .+= δ(μ) .* n
+            end
+            ifNotKeepδThenFreeδ!(μ);
+        end
+        push!(graph.backward, _meanBackward)
+    end
+    return μ
+end
+
+
+"""
+    loss(x::Variable{T}; reduction::String="sum") -> y::Variable{T}
+
+Sums or takes mean over all elements in `value` of `x` as the loss `Variable`, i.e. ⤦\n
++ `y = Variable{T}([sum(ᵛ(x))], x.backprop)`, if reduction="sum"
++ `y = Variable{T}([sum(ᵛ(x))/length(x)], x.backprop)`, if reduction="mean"
+This is very convenient for mutiple loss training. e.g. ⤦\n
+    totalLoss = β*loss(mse(x₁, ̂x₁)) + (1 - β)*loss(crossEntropy(y₁, ̂y₁))
+or in a lazy way:\n
+    totalLoss = β*mseLoss(x₁, ̂x₁) + (1 - β)*crossEntropyLoss(y₁, ̂y₁)
+where `β` is weight of mseLoss function.
+"""
+function loss(x::Variable{T}; reduction::String="sum") where T
+    by = lowercase(reduction)
+    by=="sum" && return _sum(x)
+    by=="mean" && return _mean(x)
+    @error "wrong reduction method" reduction=="sum" || reduction=="mean"
+end
+
+
+"""
+    cost(x::Variable) -> y::eltype(x)
+
+sums over all elements in `value` of `x` as the final loss value which is a scalar, i.e. ⤦\n
+    y = sum(x.value), so δ(x) = ∂y/∂x = 𝟙
+"""
 function cost(x::Variable{T}) where T
     if x.backprop
         𝟙 = eltype(x)(1.0)
