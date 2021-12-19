1f89415ff465a939c20a122496acfe6abdd4016b
sole <taiyang_zhao@163.com>
2021-12-20 02:10:12 +0800

    refactor(kit/editdistance): docs and forms
    rename from edistance to editcost

--- src/kit/4-editdistance.jl
+++ src/kit/4-editdistance.jl
@@ -1,6 +1,16 @@
-export edistance
+export editcost
 
-function edistance(tar,src)
+
+"""
+    editcost(tar, src, subcost=1)
+
+Minimum edit distance discribes how many steps to transform `src` into `tar`.
+Editing operations are `Insertion(i)`, `Deletion(d)` and `Substitution(s)`, e.g.\n
+    src: I N T E * N T I O N
+    tar: * E X E C U T I O N
+         d s s   i s
+"""
+function editcost(tar, src, subcost=1)
     T = length(tar)+1
     S = length(src)+1
     d = zeros(Int, T, S)
@@ -8,12 +18,9 @@ function edistance(tar,src)
     d[2:T,1] = 1:T-1
     for t = 2:T
         for s = 2:S
-            if src[s-1]==tar[t-1]
-                d[t,s] = d[t-1,s-1]
-            else
-                d[t,s] = 1 + min(d[t-1,s],d[t,s-1],d[t-1,s-1])
-                #                ins      del      sub
-            end
+            c = ifelse(src[s-1]!=tar[t-1], subcost, 0)
+            d[t,s] = min(d[t-1,s]+1, d[t,s-1]+1, d[t-1,s-1]+c)
+            #               ins      del         sub
         end
     end
     return d[T,S]
