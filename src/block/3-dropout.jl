705434e773c40d7906e215fa080a5afc973d4b43
sole <taiyang_zhao@163.com>
2022-01-03 14:22:56 +0800

    feat(xdropout): 切片化的dropout
    一般的dropout其掩码形状与输入相同，故而对所有位置进行随机丢弃，而xdropout的掩码形状其实是一个向量，故而可以对特定轴向的切片进行随机丢弃，没被丢弃的切片则是完好的。xdropout可以轻松地作用于特定维度，比如通道维度，时间维度（有扩增效果）

--- src/block/3-dropout.jl
+++ src/block/3-dropout.jl
@@ -1,15 +1,17 @@
 export dropout
 export dropout!
+export xdropout
+export xdropout!
 
 
 """
     dropout(x::Variable{T}; p=0.1) -> y::Variable{T}
 
-Randomly zeroes some elements of the input tensor `x` with probability `p` using samples
+Randomly zeros some elements of the input tensor `x` with probability `p` using samples
 from a Bernoulli distribution. This is an effective way to regularization and preventing
 the co-adaptation of neurons. The output elements of `y` are scaled by a factor of `1/(1-p)`
-during training. During evaluation, `dropout` should be removed. `dropout` is also viewed as
-a mean of data augmentation.
+during training. `dropout` should be removed at evaluation. Dropout is also viewed as a
+mean of data augmentation.
 """
 function dropout(x::Variable{T}; p=0.1) where T
     @assert 0.0<=p<1.0 "p is in [0,1), but got p=$p"
@@ -35,11 +37,11 @@ end
 """
     dropout!(x::Variable{T}; p=0.1) -> y::Variable{T}
 
-Randomly zeroes some elements of the input tensor `x` with probability `p` using samples
+Randomly zeros some elements of the input tensor `x` with probability `p` using samples
 from a Bernoulli distribution. This is an effective way to regularization and preventing
 the co-adaptation of neurons. The output elements of `y` are scaled by a factor of `1/(1-p)`
-during training. During evaluation, `dropout!` should be removed. `dropout` is also viewed as
-a mean of data augmentation.
+during training. `dropout!` should be removed at evaluation. Dropout is also viewed as a
+mean of data augmentation.
 """
 function dropout!(x::Variable{T}; p=0.1) where T
     @assert 0.0<=p<1.0 "p is in [0,1), but got p=$p"
@@ -47,6 +49,68 @@ function dropout!(x::Variable{T}; p=0.1) where T
     l = τ(1)
     p = τ(p)
     m = T(rand(τ, x.shape) .< (l - p)) .* (l/(l - p)) # weighted mask
+    y = Variable{T}(dotmul!(ᵛ(x), m), x.backprop)
+    if x.backprop
+        function dropoutBackward()
+            if need2computeδ!(x)
+                δ(x) .+= δ(y) .* m
+            end
+            ifNotKeepδThenFreeδ!(y);
+        end
+        push!(graph.backward, dropoutBackward)
+    end
+    return y
+end
+
+
+"""
+    xdropout(x::Variable; p=0.1, dim=1) -> y::Variable{T}
+
+Randomly zeros some `slices` of the input tensor `x` with probability `p` using samples
+from a Bernoulli distribution. This is an effective way to regularization and preventing
+the co-adaptation of neurons. The output elements of `y` are scaled by a factor of `1/(1-p)`
+during training. `xdropout` should be removed at evaluation. Dropout is also viewed as a
+mean of data augmentation.
+"""
+function xdropout(x::Variable{T}; p=0.1, dim=1) where T
+    @assert 0.0<=p<1.0 "p is in [0,1), but got p=$p"
+    τ = eltype(T)
+    l = τ(1)
+    p = τ(p)
+    N = size(x, dim)
+    S = ntuple(i -> i≠dim ? 1 : N, ndims(x))
+    m = T(rand(τ, S) .< (l - p)) .* (l/(l - p))  # weighted mask
+    y = Variable{T}(ᵛ(x) .* m, x.backprop)
+    if x.backprop
+        function dropoutBackward()
+            if need2computeδ!(x)
+                δ(x) .+= δ(y) .* m
+            end
+            ifNotKeepδThenFreeδ!(y);
+        end
+        push!(graph.backward, dropoutBackward)
+    end
+    return y
+end
+
+
+"""
+    xdropout!(x::Variable; p=0.1, dim=1) -> y::Variable{T}
+
+Randomly zeros some `slices` of the input tensor `x` with probability `p` using samples
+from a Bernoulli distribution. This is an effective way to regularization and preventing
+the co-adaptation of neurons. The output elements of `y` are scaled by a factor of `1/(1-p)`
+during training. `xdropout!` should be removed at evaluation. Dropout is also viewed as a
+mean of data augmentation.
+"""
+function xdropout!(x::Variable{T}; p=0.1, dim=1) where T
+    @assert 0.0<=p<1.0 "p is in [0,1), but got p=$p"
+    τ = eltype(T)
+    l = τ(1)
+    p = τ(p)
+    N = size(x, dim)
+    S = ntuple(i -> i≠dim ? 1 : N, ndims(x))
+    m = T(rand(τ, S) .< (l - p)) .* (l/(l - p))  # weighted mask
     y = Variable{T}(dotmul!(ᵛ(x), m), x.backprop)
     if x.backprop
         function dropoutBackward()
